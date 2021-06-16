f5a5fc0edcc1327e07c2905fffbc11e35f284648
jfremond <jfremond@student.42.fr>
2021-06-16 20:13:42 +0200

    Adding monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -4,9 +4,9 @@
 ARCHITECTURE=$(uname -a)
 PHYSICAL_CPU=$(grep "physical id" /proc/cpuinfo | sort -u | wc -l)
 VIRTUAL_CPU=$(grep -c "cpu cores" /proc/cpuinfo)
-MEM_USED=$(free | grep Mem | awk '{print $3}')
-TOTAL_MEM=$(free | grep Mem | awk '{print $2}')
-PERCENT_MEM=$(awk -v var1=$MEM_USED -v var2=$TOTAL_MEM 'BEGIN {print(var1/var2)*100}')
+MEM_USED=$(free --mega | grep Mem | awk '{print $3}')
+TOTAL_MEM=$(free --mega | grep Mem | awk '{print $2}')
+PERCENT_MEM=$(free --mega | grep Mem | awk '{print ($3/$2) * 100}')
 
 # Function that prints what I need to print
 function_to_print()
@@ -14,8 +14,7 @@ function_to_print()
 	echo "\t#Architecture : $ARCHITECTURE"
 	echo "\t#CPU physical : $PHYSICAL_CPU"
 	echo "\t#vCPU : $VIRTUAL_CPU"
-	echo "\t#Memory Usage : $MEM_USED/$TOTAL_MEM"
-	echo "\t#Percentage : $PERCENT_MEM%"
+	echo "\t#Memory Usage : "$MEM_USED/$TOTAL_MEM"MB ($PERCENT_MEM%)"
 }
 
 #Calling the function

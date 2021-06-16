26bb521ba314ac9f8eeb4832c58c69df5c41576d
jfremond <jfremond@student.42.fr>
2021-06-16 14:15:28 +0200

    Adding monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -6,7 +6,7 @@ PHYSICAL_CPU=$(grep "physical id" /proc/cpuinfo | sort -u | wc -l)
 VIRTUAL_CPU=$(grep -c "cpu cores" /proc/cpuinfo)
 MEM_USED=$(free | grep Mem | awk '{print $3}')
 TOTAL_MEM=$(free | grep Mem | awk '{print $2}')
-PERCENT_MEM=$(awk -v var1=$MEM_USED -v var2=$TOTAL_MEM 'BEGIN { print  ( var1 / var2 ) }')
+PERCENT_MEM=$(awk -v var1=$MEM_USED -v var2=$TOTAL_MEM 'BEGIN {print(var1/var2)*100}')
 
 # Function that prints what I need to print
 function_to_print()

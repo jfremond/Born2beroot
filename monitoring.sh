7acc3d57f88b66e64e1da982ba2e0e791b3ffd82
jfremond <jfremond@student.42.fr>
2021-06-20 14:17:39 +0200

    Adding monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Set character encoding
+#Set character encoding
 export LANG=C.UTF-8
 
 # Getting variables
@@ -14,6 +14,7 @@ TOTAL_DISK=$(df | awk '{print $2}' | grep [0-9] | paste -sd+ - | bc)
 DISK_USED=$(df | awk '{print $3}' | grep [0-9] | paste -sd+ - | bc)
 PERCENT_DISK=$(df | awk '{print ($3/$2) * 100}' | grep [0-9] | paste -sd+ - | bc)
 BOOT=$(who -b)
+USER_LIST=$(cut -d: -f1 /etc/passwd)
 
 # Function that prints what I need to print
 function_to_print()
@@ -24,6 +25,7 @@ function_to_print()
 	echo "\t#Memory Usage : "$MEM_USED/$TOTAL_MEM"MB ($PERCENT_MEM%)"
 	echo "\t#Disk usage : "$DISK_USED/$TOTAL_DISK" ($PERCENT_DISK%)"
 	echo "\t#Last boot : $BOOT"
+	echo "\t#Number users : $(USER_LIST)" #WRONG
 }
 
 #Calling the function
